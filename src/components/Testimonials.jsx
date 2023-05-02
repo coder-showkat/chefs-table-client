26ecf34e7afdc8abb31c19bd7502ee43b76038eb
coder-showkat <showkat.coder@gmail.com>
2023-05-02 20:01:19 +0600

    changing website title and icon

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Testimonials = () => {
+  return <div className="my-20 py-8">Testimonials</div>;
+};
+
+export default Testimonials;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../components/About";
 import ChefProfiles from "../components/ChefProfiles";
 import Hero from "../components/Hero";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <About />
       <ChefProfiles />
+      <Testimonials />
     </div>
   );
 };
